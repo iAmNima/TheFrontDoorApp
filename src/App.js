ff3cb270f80e842a77c0b2684392e24e2244645f
iamnima <nima.bayat227@gmail.com>
2021-10-19 18:38:59 +0200

    modified availability card format

--- src/App.js
+++ src/App.js
@@ -14,42 +14,69 @@ const App = () => {
   useEffect(() => {
     let time = new Date();
     setDay(time.toLocaleDateString());
-    //------getting data from database when the component renders:
+    // --> getting data from database when the component renders:
     axios
-      .get("http://localhost:5000/reservations/")
+      .get("http://localhost:5001/reservations/")
       .then((Response) => {
-        console.log(Response.data);
-        setReservation(Response.data);
+        setReservation(formatAvailabilities(setAvailabilities(Response.data)));
+        //setReservation(setAvailabilities(Response.data));
+        //setReservation(Response.data);
+        console.log("hi");
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [day]);
+
+  function setAvailabilities(reservations) {
+    let temp = [];
+    reservations.forEach((reservation) => {
+      if (reservation.day === day && reservation.roomNr == 1) {
+        temp.push(reservation);
+      }
+    });
+    return temp;
+  }
+
+  console.log(reservations);
+
+  // The MAGIC!
+  function formatAvailabilities(availabilities) {
+    availabilities.forEach((reservation) => {
+      availabilities.forEach((r2) => {
+        if (r2 != reservation) {
+          if (reservation.name == r2.name) {
+            let index_r2 = availabilities.indexOf(r2);
+            reservation.timeSlot += " | " + r2.timeSlot;
+            availabilities.splice(index_r2, 1);
+          }
+        }
+      });
+    });
+    return availabilities;
+  }
 
   return (
     <div className="mt-5">
       {reservations.map((reservation) => {
-        if (reservation.day === day && reservation.roomNr == 1) {
-          // only returning resurvations for that day:
-          // noReservation = false;
-          return (
-            <motion.div
-              key={reservation._id}
-              className="reservation-card p-3 mx-auto"
-              initial={{ x: "100vw" }}
-              animate={{ x: 0 }}
-              transition={{ type: "spring", stiffness: 50 }}
-            >
-              <div className="d-flex m-2">
-                <img className="default_profile_pic" src={profile_pic} />
-                <h3 className="my-auto ml-2">{reservation.name}</h3>
-              </div>
-              <div className="d-flex m-2">
-                <h4 className="mr-auto">{reservation.timeSlot}</h4>
-              </div>
-            </motion.div>
-          );
-        }
+        // noReservation = false;
+        return (
+          <motion.div
+            key={reservation._id}
+            className="reservation-card p-3 mx-auto shadow"
+            initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            transition={{ type: "spring", stiffness: 50 }}
+          >
+            <div className="d-flex m-2">
+              <img className="default_profile_pic" src={profile_pic} />
+              <h3 className="my-auto ml-2">{reservation.name}</h3>
+            </div>
+            <div className="d-flex m-2">
+              <h4 className="mr-auto">{reservation.timeSlot}</h4>
+            </div>
+          </motion.div>
+        );
       })}
     </div>
   );
